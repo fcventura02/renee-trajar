7ed83142590438144994b346a9010aac94d86c5b
fcventura02 <filipecvr4@gmail.com>
2021-12-26 21:34:46 -0300

    Refatoring import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Profile } from "./components/Profile";
-import { Banner } from "./components/Banner";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { LoyalClient } from "./components/LoyalClient";
-import { Objective } from "./components/Objective";
-import { Promotion } from "./components/Promotion";
-import { Products } from "./components/Products";
-import { Testimony } from "./components/Testimony";
-import { Contacts } from "./components/Contacts";
+import {
+  Profile,
+  Banner,
+  Footer,
+  Header,
+  LoyalClient,
+  Objective,
+  Promotion,
+  Products,
+  Testimony,
+  Contacts,
+} from "./components";
 
 function App() {
   return (
---
+++ src/components/index.ts
@@ -0,0 +1,22 @@
+import { Banner } from "./Banner";
+import { Contacts } from "./Contacts";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { LoyalClient } from "./LoyalClient";
+import { Objective } from "./Objective";
+import { Products } from "./Products";
+import { Profile } from "./Profile";
+import { Promotion } from "./Promotion";
+import { Testimony } from "./Testimony";
+export {
+  Banner,
+  Contacts,
+  Footer,
+  Header,
+  LoyalClient,
+  Objective,
+  Products,
+  Profile,
+  Promotion,
+  Testimony,
+};
