d8c2a798283745a703a9c7014ebf0523392b7760
fcventura02 <filipecvr4@gmail.com>
2021-12-26 21:24:22 -0300

    Created footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./footer.module.scss";
+import logo from "../../img/logo2.svg";
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.container} container`}>
+        <div className={styles.enterprise}>
+          <img height="57px" src={logo} alt="Renee Trajar logo" />
+          <span>
+            CNPJ<span>:04.487.685/0001-01</span>
+          </span>
+        </div>
+        <div>
+          <span>
+            Copyright: 2021 © Renee Trajar - Todos os direitos reservados.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+}
